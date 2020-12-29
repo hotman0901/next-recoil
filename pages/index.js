ec1371deb5d694081558f9da351ddfad198bacda
bennyhsieh <benny.hsieh@jyintgroup.com>
2020-12-29 16:39:32 +0800

    feat: update

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-import Counter from '@/components/counter';
+import Counter from '@/containers/counter';
+import { withTranslation } from '@/i18n';
 
 const IndexPage = () => <Counter />;
 
-export default IndexPage;
+IndexPage.getInitialProps = async ({ req }) => {
+  const isServer = !!req;
+  return { namespacesRequired: ['common', 'i18nSample'], isServer };
+};
+
+export default withTranslation('i18nSample')(IndexPage);
